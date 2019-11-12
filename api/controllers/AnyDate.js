525d03a2364825f5bfca212dde2eeb10b0865cd0
hongquan <hongquanhak@gmail.com>
2019-11-12 09:26:46 +0700

    fix

--- api/controllers/AnyDate.js
+++ api/controllers/AnyDate.js
@@ -4,7 +4,7 @@ const db = require('../database');
 
 module.exports = {
     get: (req, res) => {
-        let sql = `SELECT * FROM get_api WHERE DATE(time) = DATE('2019-10-21')`;
+        let sql = `SELECT * FROM get_api WHERE DATE(time) = DATE('2019-11-01')`;
         db.query(sql, (err, response) => {
             if(err) throw err;
             res.status(200);
