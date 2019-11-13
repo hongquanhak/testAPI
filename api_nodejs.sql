c0ac89f6e01703fe6ceadba01b9c6b7142614054
hongquan <hongquanhak@gmail.com>
2019-11-13 09:17:55 +0700

    Add file sql

---
+++ api_nodejs.sql
@@ -0,0 +1,7 @@
+CREATE TABLE `get_api` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `time` varchar(100) DEFAULT NULL,
+  `currency` varchar(100) DEFAULT NULL,
+  `rate` varchar(100) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=43146 DEFAULT CHARSET=latin1
